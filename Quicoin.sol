4f9b61cc77d54e78dc0ff5ee61d37eec90645ac0
quicoindeveloper <quicoindeveloper@proton.me>
2026-09-04 14:07:11 +0200

    Cambio del nombre del contrato inicial

--- Quicoin.sol
+++ Quicoin.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.22 <0.9.0;
  
 import "@openzeppelin/contracts@5.0.2/token/ERC20/ERC20.sol";
  
-contract StoneToken is ERC20 {
+contract Quicoin is ERC20 {
   constructor() ERC20("En un lugar de la mancha, de cuyo nombre no quiero acordarme...", "QUICOIN") {
         _mint(address(this), 100000000 * (10 ** uint256(decimals())));
         _approve(address(this), msg.sender, totalSupply());
